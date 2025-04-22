df7548338bb6afbcc99cef3819672388c05864e8
MateenIqbal1 <mateeniqbal840@gmail.com>
2025-04-22 16:09:48 +0500

    pushing sigin page

--- src/routes/SignInPage/SigninPage.jsx
+++ src/routes/SignInPage/SigninPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './signinpage.css';
+import './signinPage.css';
 import axios from 'axios'
 import { useAuth } from '../../context/AuthContext';
 import { SyncLoader } from 'react-spinners';
