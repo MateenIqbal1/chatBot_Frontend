8b0161c7faedf2269221d68f29e548682a25e354
MateenIqbal1 <mateeniqbal840@gmail.com>
2025-04-22 16:07:08 +0500

    fixing signup page

--- src/routes/SignupPage/SignupPage.jsx
+++ src/routes/SignupPage/SignupPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
-import './signuppage.css';
+import './signupPage.css';
 import {SyncLoader} from 'react-spinners'
 
 const SignupPage = () => {
