604cb63772253a57bd308d399e0b4156cbb3b4d0
MateenIqbal1 <mateeniqbal840@gmail.com>
2025-04-29 00:02:03 +0500

    fixing

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -14,12 +14,10 @@ const ChatList = () => {
     queryFn: () =>
       fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
         headers: {
-          Authorization: token ? `Bearer ${token}` : '',
+          Authorization: token ? `Bearer ${token}` :'',
         },
       }).then((res) => res.json()),
-    enabled: !!token, 
   });
-  
 
 
   if (!isAuthenticated) {
